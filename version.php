aebc9eb3fa104c9c98de6c46ec30cb995f291fe0
sestrauss <sstrauss@haverford.edu>
2023-04-12 10:15:41 -0400

    updated version and changelog info

--- version.php
+++ version.php
@@ -33,8 +33,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_hc';
-$plugin->version = 2023032801;
-$plugin->release = 'v3.0beta';
+$plugin->version = 2023041201;
+$plugin->release = 'v4.1.0';
 $plugin->requires = '2022111800';   
 $plugin->dependencies = [
     'theme_boost' => '2022112800'
